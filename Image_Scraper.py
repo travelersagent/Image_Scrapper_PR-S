b9b9655df1c50a097211eb46fcd5d5aec51b547a
august <august@macs-air.home>
2021-12-22 20:25:24 -0500

    image scrapper python

--- Image_Scraper.py
+++ Image_Scraper.py
@@ -1,1 +1,7 @@
-#lmao
+import requests
+image_url = input("Enter URL: ")
+image = requests.get(image_url)
+image_name = input("Enter name of image: ")
+image_type = input("Enter image file type: ")
+with open(f'{image_name}.{image_type}',"wb") as file:
+    file.write(image.content)
